9f2d6c1ecfdf86a827004b47fda4a999b239de1d
Ian Irving <ian@falsepositives.com>
2019-04-17 10:56:45 -0400

    add ember-cli-string-helpers and html-safe helper to pass thu html uicode for view link-to

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    'ember-cli-string-helpers': {
+      only: ['html-safe']
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
